0bbb1e8d4c6bf84060ea358498499ab756de134e
okakyo <0622okakyo@gmail.com>
2019-09-01 16:55:28 +0900

    ディレクトリ配置を変更

--- ex-bff/app.js
+++ ex-bff/app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// 名前を随時変更する
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -19,8 +21,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter);     // ここでフロントエンドの画面を表示する
+app.use('/api', usersRouter);  //
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- ex-bff/routes/users.js
+++ ex-bff/routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var axios=require('axios');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json({
+    _id: 1,
+    status_code:"0000",
+    message:"こんにち殺法"
+  });
 });
 
 module.exports = router;
